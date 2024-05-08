4db9d63a2360e58cee2b011dd52fea3a484475b5
Emanuel Gutierrez <emanuelgtz02@gmail.com>
2024-05-07 19:36:06 -0700

    Added loop for application

--- TaskModule/__init__.py
+++ TaskModule/__init__.py
@@ -2,3 +2,4 @@ from .Task import Task
 from .TaskReminder import Reminder
 from .TaskList import TaskList
 from .ReminderList import ReminderList
+from .TaskListView import TaskListView
--- main.py
+++ main.py
@@ -1,19 +1,64 @@
 from TaskModule.Task import Task
+from TaskModule.TaskList import TaskList
 from TaskModule.TaskReminder import Reminder
 from TaskModule.ReminderList import ReminderList
+from TaskModule.TaskListView import TaskListView
 import datetime as dt
 
 def main():
     t1 = Task("task1", dt.datetime.today(), "task1_description")
-    t2 = Task("task2", dt.datetime.today() + dt.timedelta(1), "task1_description")
+    t2 = Task("task2", dt.datetime.today() + dt.timedelta(1), "task2_description")
     t2 = Task("task3", dt.datetime.today() + dt.timedelta(1), "task1_description")
-    r1 = Reminder(t1, t1.due_date - dt.timedelta(10))
-    r2 = Reminder(t2, t2.due_date - dt.timedelta(4))
-    r3 = Reminder(t2, t2.due_date - dt.timedelta(4))
-    rl = ReminderList()
-    rl.add_reminder(r1)
-    rl.add_reminder(r2)
-    rl.add_reminder(r3)
-    s = rl.check_reminders()
-    print(s)
+
+    rem_list = ReminderList()
+    task_list = TaskList()
+    task_view = TaskListView(task_list)
+    task_list.add_task(t1)
+    task_list.add_task(t2)
+    print("===TO DO LIST / REMINDER APP")
+    while(True):
+        printSelectionMenu()
+        choice = input()
+        while not choice.isnumeric() or int(choice) > 5 or int(choice) < 0:
+            print("Please enter a valid number")
+            choice = input()
+        i_choice = int(choice)
+        if i_choice == 0:
+            return
+        elif i_choice == 1:
+            print(task_view.viewTasks())
+        elif i_choice == 2:
+            format = "%m-%d-%Y"
+            name = input("Enter the name of your task: ")
+            res = False
+            date = None
+            while not res:
+                date = input ("Enter date in mm-dd-yyyy format: ")
+                try:
+                    res = dt.datetime.strptime(date, format)
+                except ValueError:
+                    res = False
+                print(type(res))
+            desc = input("Enter a description for this task: ")
+            task_list.add_task(Task(name, date, desc))
+        elif i_choice == 3:
+            print(task_view.viewTasks())
+            del_choice = input("Enter the ID of the task you'd like to delete: ")
+            while not del_choice.isnumeric() or int(del_choice) >= len(task_list.tasks) or int(del_choice) < 0:
+                print("Please enter a valid number")
+                del_choice = input()
+            task_list.remove_task_at_index(int(del_choice))
+            
+
+
+
+
+def printSelectionMenu():
+    print("Enter a number corresponding to the option you'd like to perform")
+    print("1. View your tasks")
+    print("2. Add a task")
+    print("3. Remove a task")
+    print("4. Add a reminder to a task")
+    print("5. View reminders")
+    print("0. EXIT")
 main()
